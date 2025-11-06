41566e7966c2aed6677297cfbd3b5e3c65497501
MargotL3869 <margot.laignel8@gmail.com>
2025-11-06 12:40:44 +0100

    ajout fonction cleaning test Julia

---
+++ test/test_validation.jl
@@ -0,0 +1,22 @@
+
+
+#test d'enforce_type
+
+@testset "enforce_types basic tests" begin
+    df = DataFrame(
+        a = ["1", "2", "3", "x", missing],
+        b = ["chat", "chien", "chat", "souris", "chien"],
+        c = ["", " ", "4", "5", "6"]
+    )
+
+    df2 = enforce_types(df)
+
+    # Vérifie que a est numérique
+    @test eltype(df2.a) <: Union{Missing, Int}
+
+    # Vérifie que b est catégorielle
+    @test isa(df2.b, CategoricalVector)
+
+    # Vérifie que c est numérique (car majoritairement nombres)
+    @test eltype(df2.c) <: Union{Missing, Float64}
+end
